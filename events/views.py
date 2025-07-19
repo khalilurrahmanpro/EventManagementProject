6fe967e742039ab2ea1b2ce89804f0377dfc2eef
khalilurrahmanpro <mdkhlilurrahmanpro@gmail.com>
2025-07-19 21:11:06 +0600

    make folder template ,event

--- events/views.py
+++ events/views.py
@@ -3,6 +3,10 @@ from .models import Event, Category
 from .forms import EventForm
 from django.db.models import Q
 
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    return render(request, 'events/event_detail.html', {'event': event})
+
 def event_list(request):
     query = request.GET.get('q')
     if query:
@@ -11,7 +15,7 @@ def event_list(request):
         )
     else:
         events = Event.objects.all()
-    return render(request, 'event_list.html', {'events': events})
+    return render(request, 'events/event_list.html', {'events': events})
 
 def event_create(request):
     if request.method == 'POST':
