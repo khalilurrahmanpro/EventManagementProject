b75d404df6d112b4f90c8a889823cdf96bc1cc34
khalilurrahmanpro <mdkhlilurrahmanpro@gmail.com>
2025-07-20 17:20:25 +0600

    added event_datails

--- event_system/urls.py
+++ event_system/urls.py
@@ -7,6 +7,7 @@ from events import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.event_list, name='event_list'),
+    path('event/<int:pk>/', views.event_detail, name='event_detail'),
     path('add/', views.event_create, name='event_create'),
     path('edit/<int:pk>/', views.event_update, name='event_update'),
     path('delete/<int:pk>/', views.event_delete, name='event_delete'),
